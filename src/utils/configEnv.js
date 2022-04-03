b80c893b7af8f5d4cc7eb9655596ecbf5cfa0563
Ahsan Shah <shahahsan56@gmail.com>
2022-04-03 21:06:23 +0500

    contract added

--- src/utils/configEnv.js
+++ src/utils/configEnv.js
@@ -1,11 +1,11 @@
 export const configEnv = {
-	CONTRACT_ADDRESS: '0xefabb52ba190605800aad47ba3833e572c1d2241',
+	CONTRACT_ADDRESS: '0x2B87993FeeF4cae9590f9f543C25fAF0D364f26F',
 	AVAX_CHAINID: 43113,
 	// AVAX_CHAINID:43114, // for mainnet
 	AVAX_RPC: 'https://api.avax-test.network/ext/bc/C/rpc', //For testnet
 	// AVAX_RPC:"https://api.avax.network/ext/bc/C/rpc", //For mainnet
 	AVAX_CONTRACT_ADDRESS_URL:
-		'https://testnet.snowtrace.io/address/0xefabb52ba190605800aad47ba3833e572c1d2241#code', //Change for mainnet,
+		'https://testnet.snowtrace.io/address/0x2B87993FeeF4cae9590f9f543C25fAF0D364f26F#code', //Change for mainnet,
 	AVAX_NETWORK_ID: 'avax',
 	AVAX_NETWORK_NAME: 'Avax Mainnet',
 	REF_ADDRESS: '0x0000000000000000000000000000000000000000',
