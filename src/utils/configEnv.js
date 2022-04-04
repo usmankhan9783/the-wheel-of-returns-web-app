de1f1453b0177258edcd2eeb64abe39e98e41e1c
Muhammad Usman <usman@solissol.com>
2022-04-04 22:36:43 +0500

    address changed

--- src/utils/configEnv.js
+++ src/utils/configEnv.js
@@ -1,11 +1,11 @@
 export const configEnv = {
-	CONTRACT_ADDRESS: '0x9657b24bde1d1e343aa4ef5a517aee0ee78d9f4a',
+	CONTRACT_ADDRESS: '0xa37e36e04db5de10e0336b706e4d544285f8f417',
 	// AVAX_CHAINID: 43113,
 	AVAX_CHAINID:43114, // for mainnet
 	// AVAX_RPC: 'https://api.avax-test.network/ext/bc/C/rpc', //For testnet
 	AVAX_RPC:"https://api.avax.network/ext/bc/C/rpc", //For mainnet
 	AVAX_CONTRACT_ADDRESS_URL:
-		'https://snowtrace.io/address/0x9657b24bde1d1e343aa4ef5a517aee0ee78d9f4a#code', //Change for mainnet,
+		'https://snowtrace.io/address/0xa37e36e04db5de10e0336b706e4d544285f8f417#code', //Change for mainnet,
 	AVAX_NETWORK_ID: 'avax',
 	AVAX_NETWORK_NAME: 'Avax Mainnet',
 	REF_ADDRESS: '0x0000000000000000000000000000000000000000',
