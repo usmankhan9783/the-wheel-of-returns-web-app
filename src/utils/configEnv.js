31d4adb9a42fd1414ad5dff8894ac183907e363e
Ahsan Shah <shahahsan56@gmail.com>
2022-04-04 20:05:52 +0500

    changed contract address and abi

--- src/utils/configEnv.js
+++ src/utils/configEnv.js
@@ -1,11 +1,11 @@
 export const configEnv = {
-	CONTRACT_ADDRESS: '0xdfbb6953a1b6720020a16A1BDa1a826af4554512',
+	CONTRACT_ADDRESS: '0x9657b24bde1d1e343aa4ef5a517aee0ee78d9f4a',
 	// AVAX_CHAINID: 43113,
 	AVAX_CHAINID:43114, // for mainnet
 	// AVAX_RPC: 'https://api.avax-test.network/ext/bc/C/rpc', //For testnet
 	AVAX_RPC:"https://api.avax.network/ext/bc/C/rpc", //For mainnet
 	AVAX_CONTRACT_ADDRESS_URL:
-		'https://snowtrace.io/address/0xdfbb6953a1b6720020a16A1BDa1a826af4554512#code', //Change for mainnet,
+		'https://snowtrace.io/address/0x9657b24bde1d1e343aa4ef5a517aee0ee78d9f4a#code', //Change for mainnet,
 	AVAX_NETWORK_ID: 'avax',
 	AVAX_NETWORK_NAME: 'Avax Mainnet',
 	REF_ADDRESS: '0x0000000000000000000000000000000000000000',
