d80d9e2c93f39bfea66c3cda04d790cd0ee6a0de
Ahsan Shah <shahahsan56@gmail.com>
2022-04-04 19:48:36 +0500

    changed testnet contract address

--- src/utils/configEnv.js
+++ src/utils/configEnv.js
@@ -1,11 +1,11 @@
 export const configEnv = {
-	CONTRACT_ADDRESS: '0x2B87993FeeF4cae9590f9f543C25fAF0D364f26F',
+	CONTRACT_ADDRESS: '0x863cE452Af926A3dC928685602db458b8F163948',
 	AVAX_CHAINID: 43113,
 	// AVAX_CHAINID:43114, // for mainnet
 	AVAX_RPC: 'https://api.avax-test.network/ext/bc/C/rpc', //For testnet
 	// AVAX_RPC:"https://api.avax.network/ext/bc/C/rpc", //For mainnet
 	AVAX_CONTRACT_ADDRESS_URL:
-		'https://testnet.snowtrace.io/address/0x2B87993FeeF4cae9590f9f543C25fAF0D364f26F#code', //Change for mainnet,
+		'https://testnet.snowtrace.io/address/0x863cE452Af926A3dC928685602db458b8F163948#code', //Change for mainnet,
 	AVAX_NETWORK_ID: 'avax',
 	AVAX_NETWORK_NAME: 'Avax Mainnet',
 	REF_ADDRESS: '0x0000000000000000000000000000000000000000',
